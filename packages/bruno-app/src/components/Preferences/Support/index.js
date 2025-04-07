28e9a6c87ba75950548f5cb305c9780f79d2080a
Gary Ascuy <gary.ascuy@gmail.com>
2025-04-06 22:32:20 -0500

    Updating support links

--- packages/bruno-app/src/components/Preferences/Support/index.js
+++ packages/bruno-app/src/components/Preferences/Support/index.js
@@ -10,33 +10,21 @@ const Support = () => {
     <StyledWrapper>
       <div className="rows">
         <div className="mt-2">
-          <a href="https://docs.usebruno.com" target="_blank" className="flex items-end">
+          <a href="https://home.nekoverse.me/" target="_blank" className="flex items-end">
             <IconBook size={18} strokeWidth={2} />
             <span className="label ml-2">{t('COMMON.DOCUMENTATION')}</span>
           </a>
         </div>
         <div className="mt-2">
-          <a href="https://github.com/usebruno/bruno/issues" target="_blank" className="flex items-end">
+          <a href="https://github.com/nekoverse-api/neko-api-tools/issues" target="_blank" className="flex items-end">
             <IconSpeakerphone size={18} strokeWidth={2} />
             <span className="label ml-2">{t('COMMON.REPORT_ISSUES')}</span>
           </a>
         </div>
         <div className="mt-2">
-          <a href="https://discord.com/invite/KgcZUncpjq" target="_blank" className="flex items-end">
-            <IconBrandDiscord size={18} strokeWidth={2} />
-            <span className="label ml-2">{t('COMMON.DISCORD')}</span>
-          </a>
-        </div>
-        <div className="mt-2">
-          <a href="https://github.com/usebruno/bruno" target="_blank" className="flex items-end">
+          <a href="https://github.com/nekoverse-api/neko-api-tools" target="_blank" className="flex items-end">
             <IconBrandGithub size={18} strokeWidth={2} />
             <span className="label ml-2">{t('COMMON.GITHUB')}</span>
-          </a>
-        </div>
-        <div className="mt-2">
-          <a href="https://twitter.com/use_bruno" target="_blank" className="flex items-end">
-            <IconBrandTwitter size={18} strokeWidth={2} />
-            <span className="label ml-2">{t('COMMON.TWITTER')}</span>
           </a>
         </div>
       </div>
